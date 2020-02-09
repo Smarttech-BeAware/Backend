ce3e4e17f0d7c62c67b6abc0ab0055784e98ec0f
Adebiyi Emmanuel <smartemma03@gmail.com>
2020-02-09 06:22:43 +0100

    seed data via extension method

--- BeAware/MyExtensions.cs
+++ BeAware/MyExtensions.cs
@@ -27,11 +27,114 @@ namespace BeAware
                     var services = scope.ServiceProvider;
                     var dbContext = services.GetRequiredService<AppDbContext>();
 
+                    //seed user data
+                    dbContext.Database.EnsureCreated();
+                    var user = SeedUserData();
+                    dbContext.Users.Add(user);
+                    dbContext.SaveChanges();
+                    //end seed
+
                     dbContext.Database.Migrate();
                 }
             }
 
             return webHost;
+        }
+
+        public static User SeedUserData()
+        {
+            var user = new User()
+            {
+                Name = "Emmanuel",
+                Email = "emmanuel@smart.com",
+                Password = "emmanuel@smart.com",
+                Devices = new List<Device>()
+                {
+                    new Device()
+                    {
+                        Name = "Rita",
+                        WhatsApp = "Hello What's up",
+                        Messenger = "How are you doing?",
+                        Map = new List<Location>()
+                        {
+                            new Location()
+                            {
+                                Latitude = "6.5537807",
+                                Longitude = "3.366543",
+                                Time = "random time"
+                            },
+                            new Location()
+                            {
+                                Latitude = "6.6141927",
+                                Longitude = "3.3562408",
+                                Time = "random time"
+                            },
+                            new Location()
+                            {
+                                Latitude = "6.5514361",
+                                Longitude = "3.3746513",
+                                Time = "random time"
+                            }
+                        }
+                    },
+                    new Device()
+                    {
+                        Name = "Ekene",
+                        WhatsApp = "Hello What's up",
+                        Messenger = "How are you doing?",
+                        Map = new List<Location>()
+                        {
+                            new Location()
+                            {
+                                Latitude = "7.6125908",
+                                Longitude = "5.1670672",
+                                Time = "random time"
+                            },
+                            new Location()
+                            {
+                                Latitude = "7.7795976",
+                                Longitude = "5.3132664",
+                                Time = "random time"
+                            },
+                            new Location()
+                            {
+                                Latitude = "7.5908088",
+                                Longitude = "4.9778706",
+                                Time = "random time"
+                            }
+                        }
+                    },
+                    new Device()
+                    {
+                        Name = "Doyin",
+                        WhatsApp = "Hello What's up",
+                        Messenger = "How are you doing?",
+                        Map = new List<Location>()
+                        {
+                            new Location()
+                            {
+                                Latitude = "8.4990397",
+                                Longitude = "4.5838174",
+                                Time = "random time"
+                            },
+                            new Location()
+                            {
+                                Latitude = "8.4928467",
+                                Longitude = "4.5940338",
+                                Time = "random time"
+                            },
+                            new Location()
+                            {
+                                Latitude = "8.4822187",
+                                Longitude = "4.5445378",
+                                Time = "random time"
+                            }
+                        }
+                    }
+
+                }
+            };
+            return user;
         }
     }
 }
